bb30490a7415763b8aeef8c58c1807e1c2d296d8
Ivanna Navrotska <navrockaivanna15@gmail.com>
2024-03-03 16:52:44 +0200

    Update code.py

--- lab_1/code.py
+++ lab_1/code.py
@@ -1,12 +1,28 @@
-a = int(input('give me any integer: '))
-b = int(input('i need one more to find gcd: '))
+answer = input("gcd or fibonacci? put 'gcd' or 'fib': ")
 
-def gcd (a,b):
-    if a == 0 and b == 0:
-        return
-    elif b == 0:
-        return a
-    else:
-        return gcd(b, a%b)
+if answer == 'gcd':
 
-print(f'here is gcd of those integers: {gcd(a,b)}')
+    a = int(input('give me any integer: '))
+    b = int(input('i need one more to find gcd: '))
+
+    def gcd (a,b):
+        if a == 0 and b == 0:
+            return
+        elif b == 0:
+            return a
+        else:
+            return gcd(b, a%b)
+
+    print(f'here is gcd of those integers: {gcd(a,b)}')
+
+else:
+
+    f = int(input('give me num of fibonacci you wanna know: '))
+
+    def fibonacci (f):
+        if f == 0 or f == 1:
+            return 1
+        else:
+            return fibonacci(f-1) + fibonacci(f-2)
+    
+    print(f'here is your fibonacci: {fibonacci(f)}')  
