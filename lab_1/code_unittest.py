7fe032433090ad9bbc729a17ffa7681acc591dab
Ivanna Navrotska <navrockaivanna15@gmail.com>
2024-03-03 17:08:07 +0200

    Create code_unittest.py

---
+++ lab_1/code_unittest.py
@@ -0,0 +1,32 @@
+import unittest
+import code
+
+class TestCode(unittest.TestCase):
+
+    def test_gcd(self):
+
+        result = code.gcd(12,6)
+        self.assertEqual(result, 6)
+
+        result = code.gcd(0,0)
+        self.assertEqual(result, None)
+
+        result = code.gcd(-15,0)
+        self.assertEqual(result, -15)
+
+        result = code.gcd(17,32)
+        self.assertEqual(result, 1)
+
+    def test_fibonacci(self):
+
+        result = code.fibonacci(0)
+        self.assertEqual(result, 1)
+
+        result = code.fibonacci(5)
+        self.assertEqual(result, 8)
+
+        result = code.fibonacci(13)
+        self.assertEqual(result, 377)
+
+if __name__ == '__main__':
+    unittest.main()
